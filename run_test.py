268a6bc3dbb62d1eabeb3e4187746af72c09678f
PARthuu <parthivmaneoti@gmail.com>
2025-07-13 04:43:28 +0530

    new code run_test.py

--- run_test.py
+++ run_test.py
@@ -1,12 +1,12 @@
-from src.data_loader import load_yfinance_data, load_crypto_data
+from src.data_loader import load_data
 import src.strategy as strategy
 from src.backtester import Backtester
 from src.metrics import calculate_metrics
 from src.tracker import save_experiment
-from src.visualizer import plot_equity_curve, plot_signals, new_plot_signals, plot_trend
+from src.visualizer import show_metrics, plot_results, plot_signals
 
 
-df = load_crypto_data("BTC/USDT", timeframe="1m", limit=10000)  # or load_yfinance_data(...)
+df = load_data("BTC")
 
 strat = strategy.BollingerBandStrategy(df)
 signals = strat.generate_signals()
@@ -14,7 +14,7 @@ signals = strat.generate_signals()
 bt = Backtester(df, signals)
 results = bt.run()
 metrics = calculate_metrics(results)
+show_metrics(metrics)
 
+plot_results(df, signals, results)
 # save_experiment(metrics, tag="SMA_20_50_BTC")
-new_plot_signals(df, signals)
-plot_equity_curve(results)
