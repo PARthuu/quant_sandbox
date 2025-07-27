f9d780ee9fd5e654dd07ea796e9eb1a13dc46e94
PARthuu <parthivmaneoti@gmail.com>
2025-07-27 23:20:29 +0530

    renamed run_test.py as main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from src.data_loader import load_yfinance_data
-from src.visualizer import plot_equity_curve
-from src.strategy import SMACrossoverStrategy as st
+from src.data_loader import load_data
+from src.strategy import SwingStrategy as strategy
 from src.backtester import Backtester
 from src.metrics import calculate_metrics
-from src.visualizer import plot_signals, plot_equity_curve, plot_trend
 from src.tracker import save_experiment
+from src.visualizer import show_metrics, plot_results
 
-df = load_yfinance_data("AAPL", "2025-07-03", "2025-07-04", "1m")
 
-# print(df)
-# plot_trend(df)
-strategy = st(df)
-signals = strategy.generate_signals()
+df = load_data("BTC")
+
+strat = strategy(df)
+signals = strat.generate_signals()
+
 bt = Backtester(df, signals)
 results = bt.run()
 metrics = calculate_metrics(results)
+# show_metrics(metrics)
 
-save_experiment(metrics, tag="SMA_20_50_AAPL")
-plot_signals(df, signals)
-plot_equity_curve(results)
+plot_results(df, signals, results)
+# save_experiment(metrics, tag="SMA_20_50_BTC")
