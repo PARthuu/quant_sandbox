0e601a0564609c5ff6f8067e6fbad4d3c01aa8d1
PARthuu <parthivmaneoti@gmail.com>
2025-08-05 11:23:26 +0530

    added select_to_load_data()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.data_loader import load_data
+from src.data_loader import load_file, select_to_load_data
 from src.strategy import BollingerBandStrategy as strategy
 from src.backtester import Backtester
 from src.metrics import calculate_metrics
@@ -6,7 +6,7 @@ from src.tracker import save_experiment
 from src.visualizer import show_metrics, plot_results
 
 
-df = load_data("BTC")
+df = select_to_load_data()
 
 strat = strategy(df)
 signals = strat.generate_signals()
@@ -14,7 +14,6 @@ signals = strat.generate_signals()
 bt = Backtester(df, signals)
 results = bt.run()
 metrics = calculate_metrics(results)
-show_metrics(metrics)
-
+# show_metrics(metrics)
 plot_results(df, signals, results)
 # save_experiment(metrics, tag="SMA_20_50_BTC")
